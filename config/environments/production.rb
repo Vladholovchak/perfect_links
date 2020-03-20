cc2969edcb09461b81b2a96c71169e31d91a150d
Vladholovchak <vlad.golovchak.1996@gmail.com>
2020-03-20 14:32:13 +0200

    production mongoid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { host: 'perfectlinks.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      from: 'perfect_links@.com',
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV['username'],
+      password: ENV['password']
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
